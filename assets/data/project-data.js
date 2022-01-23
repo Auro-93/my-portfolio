7c08896aa198fb7a376b8655187ebd3d749fdaf4
Auro-93 <93a.sirigu@gmail.com>
2022-01-23 21:30:52 +0100

    .

--- assets/data/project-data.js
+++ assets/data/project-data.js
@@ -3,7 +3,7 @@ export const projects = [
     name: "TEA-SHOP",
     description: `A fullstack (MERN stack) tea e-commerce which performs main user, admin and guest 
     CRUD operations on personal profile, wishlist, cart, payments, orders, reviews, products and categories.`,
-    image: "assets/images/tea-shop.png",
+    image: "../assets/images/tea-shop.png",
     imgAlt: "tea shop website image",
     website: "https://tea-shop-frontend.netlify.app/",
     repository: "https://github.com/Auro-93/tea-shop-frontend",
@@ -11,7 +11,7 @@ export const projects = [
   {
     name: "AUDIO PLAYER",
     description: `A desktop audio player built with vanilla Javascript which provides all the main audio-controls features.`,
-    image: "assets/images/audio-player.png",
+    image: "../assets/images/audio-player.png",
     imgAlt: "audio-player website image",
     website: "https://auro-93.github.io/Audio-Player/",
     repository: "https://github.com/Auro-93/Audio-Player",
@@ -19,7 +19,7 @@ export const projects = [
   {
     name: "RECIPE APP",
     description: `A recipes-generator built with vanilla Javascript. Data are fetched from Edamam API.`,
-    image: "assets/images/recipe-app.png",
+    image: "../assets/images/recipe-app.png",
     imgAlt: "recipe-app website image",
     website: "https://auro-93.github.io/Basic-Recipe-App/",
     repository: "https://github.com/Auro-93/Basic-Recipe-App",
@@ -28,7 +28,7 @@ export const projects = [
     name: "COUNTRIES INFO APP",
     description: `Single-page application built with React.js. User can type in the search bar a country of his choice and view its information.
     Data are fetched from Rest countries API.`,
-    image: "assets/images/rest-countries.png",
+    image: "../assets/images/rest-countries.png",
     imgAlt: "countries info app website image",
     website: "https://rest-countries-app-v-2.vercel.app/#/",
     repository: "https://github.com/Auro-93/rest-countries-app",
@@ -37,7 +37,7 @@ export const projects = [
     name: "TO-DO APP",
     description: `To-do app built with React.js. User can save tasks in local storage, use drag&drop functionality
     and switch between light and dark theme.`,
-    image: "assets/images/todo.png",
+    image: "../assets/images/todo.png",
     imgAlt: "ro-do app website image",
     website: "https://auro-93.github.io/React-Todo-App/",
     repository: "https://github.com/Auro-93/React-Todo-App",
@@ -45,7 +45,7 @@ export const projects = [
   {
     name: "BLOGR - LANDING PAGE",
     description: `Mobile first and responsive landing page built with HTML, Sass, Javascript and a little bit of Jquery.`,
-    image: "assets/images/blogr.png",
+    image: "../assets/images/blogr.png",
     imgAlt: "Blogr landing page image",
     website:
       "https://auro-93.github.io/Landing-Page-Blogr---Frontend-Mentor-challenge/",
@@ -55,7 +55,7 @@ export const projects = [
   {
     name: "FYLO - LANDING PAGE",
     description: `Mobile first and responsive landing page built with HTML, CSS and a little bit of Javascript.`,
-    image: "assets/images/fylo.png",
+    image: "../assets/images/fylo.png",
     imgAlt: "Fylo landing page image",
     website:
       "https://auro-93.github.io/Landing-Page-Fylo-----Frontend-Mentor-challenge/",
